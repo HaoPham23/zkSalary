88c3f59321d5a2996702a32190146e50cf3d0a28
Pham Duc Hao <hao.pham1652003@gmail.com>
2023-08-03 11:01:22 +0700

    Front-end: Add Landing Page, Prove/Verify Form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import logo from './logo.svg';
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LandingPage from './components/pages/LandingPage'
+import LoginPage from './components/pages/LoginPage'
+import HomePage from './components/pages/HomePage'
+import Prover from './components/pages/Prover'
+import Verifier from './components/pages/Verifier'
+
+import './App.css'
+import BackgroundImage from './assets/images/bg.png'
+
+
+export default function App() {
+    return (
+        <Router>
+            <div style={AppStyle}>
+                <Switch>
+                    <Route exact path="/" component={ LandingPage } />
+                    <Route path="/login" component={ LoginPage } />
+                    <Route path="/home" component={ HomePage } />
+                    <Route path="/prove" component={ Prover}/>
+                    <Route path="/verify" component={ Verifier}/>
+                </Switch>
+            </div>
+        </Router>
+    )
 }
 
-export default App;
+const AppStyle = {
+  width: "100%",
+  height: "100vh",
+  background: `url(${BackgroundImage})`,
+  backgroundPosition: "center",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover"
+}
