82b301821f55fe5abf6e899448bb08d9c1f6457d
Pham Duc Hao <hao.pham1652003@gmail.com>
2023-08-04 18:34:18 +0700

    Modify HomePage

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,10 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import '../../App.css';
 
 const HomePage: React.FC = () => {
+  // const [formEmployer, setFormEmployer] = useState({
+  //   name: '',
+  //   identifier: '',
+  //   salary: '',
+  //   });
+  // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   const { name, value } = e.target;
+  //   setFormEmployer((prevData) => ({
+  //   ...prevData,
+  //   [name]: value,
+  //   }));
+  // };
+  // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   console.log(formEmployer);
+  //   // const { identifier, lower, upper, proof, publicSignals} = formVerify;
+  //   const {name, identifier, salary} = formEmployer;
+  // } Move to AddPage or sth
   return (
     <div className="text-center">
-      <h1 className="main-title home-page-title">Welcome to our app</h1>
+      <h1 className="main-title">Welcome</h1>
+      <div className="buttons text-center">
+        <Link to="add">
+          <button className="primary-button" id="reg_btn">
+            <span>add </span>
+          </button>
+        </Link>
+        <Link to="remove">
+          <button className="primary-button" id="reg_btn">
+            <span>remove </span>
+          </button>
+        </Link>
+        <Link to="view">
+          <button className="primary-button" id="reg_btn">
+            <span>view </span>
+          </button>
+        </Link>
+      </div>
       <Link to="/">
         <button className="primary-button">Log out</button>
       </Link>
--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,22 +1,46 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 import '../../App.css';
 
 const SignInPage: React.FC = () => {
+  const navigate = useNavigate();
+  const [formLogin, setFormLogin] = useState({
+    company: '',
+    password: '',
+    });
+  const [isValid, setIsValid] = useState(false);
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormLogin((prevData) => ({
+    ...prevData,
+    [name]: value,
+    }));
+  };
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formLogin);
+    if (formLogin.company === 'VNG' && formLogin.password === '1') {
+      setIsValid(true);
+    }
+    if (isValid === true) {
+      navigate('/home');
+    }
+    // const { identifier, lower, upper, proof, publicSignals} = formVerify;
+  }
   return (
     <div className="text-center">
       <h2 className="main-title">Sign In</h2>
-      <form action="/home">
+      <form action="/home" onSubmit={handleSubmit} >
         <p>
           <label>Company</label>
           <br />
-          <input type="text" name="company" required />
+          <input type="text" name="company" value={formLogin.company} onChange={handleChange} required />
         </p>
         <p>
           <label>Password</label>
           <br />
-          <input type="password" name="password" required />
+          <input type="password" name="password" value={formLogin.password} onChange={handleChange} required />
         </p>
         <p>
           <button id="sub_btn" type="submit">Login</button>
